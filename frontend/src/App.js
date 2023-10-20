c69da10f9f265d07001be460a7f3b115789f20dd
wasu-code <61418403+wasu-code@users.noreply.github.com>
2023-10-20 18:48:18 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/manual" element={ <h1>Manual, not component</h1> } />
+    </Routes>
+  )
+}
+
+export default App;
