d45e10a7b12ec123582175fcbca67b6167402ec5
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-10-24 22:52:08 +0200

    Create simple page structure

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,21 @@
 function Footer() {
-	return <div>Navbar</div>;
+	return (
+		<footer className="footer">
+			<nav>
+				<ul className="footer__nav">
+					<li className="footer__nav-item">O nas</li>
+					<li className="footer__nav-item">FAQ</li>
+					<li className="footer__nav-item">Profil</li>
+					<li className="footer__nav-item">Zarezerwuj wizytę</li>
+				</ul>
+			</nav>
+			<hr />
+			<div className="footer__logo">
+				<p className="footer__logo-name">GearUpScheduler</p>
+				<p className="footer__logo-copy">&copy; 2023 GearUpScheduler. Wszystkie prawa zastrzeżone</p>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,87 @@
+import { TiCalendarOutline } from "react-icons/ti";
+
 function Home() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+		<main>
+			<section className="hero">
+				<div className="hero__inner">
+					<h1 className="hero__header">Lorem ipsum</h1>
+					<p className="hero__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu facilisis odio.</p>
+
+					<button className="hero__button">Umów Wizytę</button>
+				</div>
+			</section>
+			<section className="sao">
+				<div>
+					<h2>
+						<span>
+							<TiCalendarOutline />
+						</span>
+						Umów się online
+					</h2>
+					<p>
+						GearUpScheduler to aplikacja stworzona z myślą o Tobie, aby umożliwić Ci szybkie i proste umówienie się do mechanika. Wystarczy kilka kliknięć, a
+						nasz intuicyjny kalendarz pomoże Ci zarezerwować termin bezpośrednio w aplikacji.
+					</p>
+					<p>
+						Aplikacja GearUpScheduler została zaprojektowana tak, aby proces rezerwacji był jak najprostszy. Dzięki naszemu intuicyjnemu kalendarzowi, możesz
+						łatwo wybrać termin, który najbardziej Ci odpowiada. Aplikacja jest prosta w obsłudze, więc nawet jeśli nie jesteś zaznajomiony z nowoczesnymi
+						technologiami, możesz z niej korzystać bez problemu.
+					</p>
+				</div>
+				<div>
+					<h2>
+						<span>
+							<TiCalendarOutline />
+						</span>
+						Umów się online
+					</h2>
+					<p>
+						GearUpScheduler to aplikacja stworzona z myślą o Tobie, aby umożliwić Ci szybkie i proste umówienie się do mechanika. Wystarczy kilka kliknięć, a
+						nasz intuicyjny kalendarz pomoże Ci zarezerwować termin bezpośrednio w aplikacji.
+					</p>
+					<p>
+						Aplikacja GearUpScheduler została zaprojektowana tak, aby proces rezerwacji był jak najprostszy. Dzięki naszemu intuicyjnemu kalendarzowi, możesz
+						łatwo wybrać termin, który najbardziej Ci odpowiada. Aplikacja jest prosta w obsłudze, więc nawet jeśli nie jesteś zaznajomiony z nowoczesnymi
+						technologiami, możesz z niej korzystać bez problemu.
+					</p>
+				</div>
+				<div>
+					<h2>
+						<span>
+							<TiCalendarOutline />
+						</span>
+						Umów się online
+					</h2>
+					<p>
+						GearUpScheduler to aplikacja stworzona z myślą o Tobie, aby umożliwić Ci szybkie i proste umówienie się do mechanika. Wystarczy kilka kliknięć, a
+						nasz intuicyjny kalendarz pomoże Ci zarezerwować termin bezpośrednio w aplikacji.
+					</p>
+					<p>
+						Aplikacja GearUpScheduler została zaprojektowana tak, aby proces rezerwacji był jak najprostszy. Dzięki naszemu intuicyjnemu kalendarzowi, możesz
+						łatwo wybrać termin, który najbardziej Ci odpowiada. Aplikacja jest prosta w obsłudze, więc nawet jeśli nie jesteś zaznajomiony z nowoczesnymi
+						technologiami, możesz z niej korzystać bez problemu.
+					</p>
+				</div>
+				<div>
+					<h2>
+						<span>
+							<TiCalendarOutline />
+						</span>
+						Umów się online
+					</h2>
+					<p>
+						GearUpScheduler to aplikacja stworzona z myślą o Tobie, aby umożliwić Ci szybkie i proste umówienie się do mechanika. Wystarczy kilka kliknięć, a
+						nasz intuicyjny kalendarz pomoże Ci zarezerwować termin bezpośrednio w aplikacji.
+					</p>
+					<p>
+						Aplikacja GearUpScheduler została zaprojektowana tak, aby proces rezerwacji był jak najprostszy. Dzięki naszemu intuicyjnemu kalendarzowi, możesz
+						łatwo wybrać termin, który najbardziej Ci odpowiada. Aplikacja jest prosta w obsłudze, więc nawet jeśli nie jesteś zaznajomiony z nowoczesnymi
+						technologiami, możesz z niej korzystać bez problemu.
+					</p>
+				</div>
+			</section>
+		</main>
 	);
 }
 
