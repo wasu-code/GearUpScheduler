96eedaf4859c7c77f77ad238fe2532ff243a031a
Mateusz Zientek <mateusz-z1703@o2.pl>
2023-10-28 20:47:19 +0200

    .

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,21 +1,23 @@
 function Footer() {
-	return (
-		<footer className="footer">
-			<nav>
-				<ul className="footer__nav">
-					<li className="footer__nav-item">O nas</li>
-					<li className="footer__nav-item">FAQ</li>
-					<li className="footer__nav-item">Profil</li>
-					<li className="footer__nav-item">Zarezerwuj wizytę</li>
-				</ul>
-			</nav>
-			<hr />
-			<div className="footer__logo">
-				<p className="footer__logo-name">GearUpScheduler</p>
-				<p className="footer__logo-copy">&copy; 2023 GearUpScheduler. Wszystkie prawa zastrzeżone</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <nav>
+        <ul className="footer__nav">
+          <li className="footer__nav-item">O nas</li>
+          <li className="footer__nav-item">FAQ</li>
+          <li className="footer__nav-item">Profil</li>
+          <li className="footer__nav-item">Zarezerwuj wizytę</li>
+        </ul>
+      </nav>
+      <hr />
+      <div className="footer__logo">
+        <p className="footer__logo-name">GearUpScheduler</p>
+        <p className="footer__logo-copy">
+          &copy; 2023 GearUpScheduler.Wszystkie prawa zastrzeżone
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
