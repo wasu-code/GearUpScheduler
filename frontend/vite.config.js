7db0e8801beb0d0ccdfe41552eb7af4dae088da6
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-11-05 16:30:00 +0100

    Server proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 3000,
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
     },
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 });
