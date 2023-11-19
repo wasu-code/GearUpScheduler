ce8d5cb9d09c282f481fdcf9b2828b4918fb4c83
wasu-code <61418403+wasu-code@users.noreply.github.com>
2023-11-19 21:19:06 +0100

    display visits in User Dashboard

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -20,36 +20,62 @@ import {
 } from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
 import { createContext, useContext, useEffect, useState } from "react";
-
+import { useAuth } from "@/context/AuthContext";
 
 
 export function UserDashboard() {
+  const { user } = useAuth();
+  const [visits, setVisits] = useState([]);
 
   useEffect(() => {
-    getUserVisits("655752339ca1d5838b054229"); //xx
-  }, []);
+    getUserVisits(user.id).then( (data) => {
+      setVisits(data)
+    });
+      
+  }, [user]);
 
   async function getUserVisits(id) {
     return new Promise((resolve, reject) => {
       // Make API call for login
-      fetch("api/visitGet", {
-        method: "POST",
+      fetch("api/getUserVisit?user_id="+id, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          user_id: id
-        }),
       })
         .then((response) => {
           if (response.status == 200) {
-            console.log(response.json());
             return response.json();
           } else {
-            console.log(response.json());
+            return response.text();
           }
         })
         .then((data) => {
+          /*data = [{
+            "day": new Date("2023-12-10"),
+            "startTime": 10,
+            "duration": 60,
+            "user_id": "user123",
+            "type": "example",
+            "description": "Example visit"
+          },
+          {
+            "day": new Date("2023-12-10"),
+            "startTime": 10,
+            "duration": 60,
+            "user_id": "user123",
+            "type": "example",
+            "description": "Example visit"
+          },
+          {
+            "day": new Date("2023-12-10"),
+            "startTime": 10,
+            "duration": 60,
+            "user_id": "user123",
+            "type": "example",
+            "description": "Example visit"
+          }]*/
+          console.log(data);
           resolve(data);
         })
         .catch((error) => {
@@ -59,46 +85,88 @@ export function UserDashboard() {
     });
   }
 
+  function daysLeft(date) {
+    // Convert both dates to milliseconds
+    const givenDate = new Date(date);
+    const today = new Date();
+  
+    // Calculate the difference in milliseconds
+    const difference = givenDate.getTime() - today.getTime() ;
+  
+    // Convert milliseconds to days
+    let daysDifference = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    //return max of 100 days (max value on progress bar)
+    if (daysDifference > 100) {daysDifference = 100}
+  
+    return daysDifference;
+  }
+
   return (
-    <div><br/> <br/> <br/> <br/> 
-      <h1 className="m-5 text-lg font-bold">Wizyty: NAme + SURname</h1>
-      <div className="flex items-center space-x-4 m-4">
-      {/*<Skeleton className="h-12 w-12 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>*/}
+    <div className="min-h-[80vh]"><br/> <br/> <br/> <br/> 
+      <h1 className="m-5 text-lg font-bold">Witaj {user.name}. Twoje wizyty:</h1>
+      <div className="flex flex-wrap items-start  m-4">
 
-      <Alert className="relative w-full">
-      <AlertCircle className="h-4 w-4" />
-      <AlertTitle>Zbliżająca się wizyta</AlertTitle>
-      <AlertDescription>
-        Twoja wizyta  
-        <Badge variant="outline" className="mx-2">Rodzaj wizyty</Badge> zbliża się. Pozostałao X dni
+        { visits && visits.length > 0 ? 
+          visits.map((v, index) => {
+            let daysLeft2 = daysLeft(v.day);
+          
+            return (
+              <Alert key={index} className="relative w-1/2 mx-4 my-1">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Zbliżająca się wizyta</AlertTitle>
+                <AlertDescription>
+                  Twoja wizyta  
+                  <Badge variant="outline" className="mx-2">
+                    {v.type}
+                  </Badge> 
 
-        <Progress value={33} className="my-1"/>
-        
-        <AlertDialog>
-          <AlertDialogTrigger>
-            <Button variant="outline" className="mt-2">Odwołaj wizytę</Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Jesteś pewien, że chcesz odwołać wizytę?</AlertDialogTitle>
-              <AlertDialogDescription>
-                Tej akcji nie można cofnąć. <br/> 
-                Możesz umówić nowy termin wizyty na ekranie głównym.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Anuluj</AlertDialogCancel>
-              <AlertDialogAction>Odwołaj</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </AlertDescription>
-    </Alert>
+                  {daysLeft2 > 0 ? 
+                    "zbliża się. Pozostałao "+ daysLeft2 + " dni"
+                  :
+                    "zakończyła się " + daysLeft2 + " dni temu"
+                  } 
+                  
+
+                  <Progress value={daysLeft2} className="my-1"/>
+
+                  {v.description} <br/> 
+                  
+                  <AlertDialog>
+                    <AlertDialogTrigger>
+                      <Button variant="outline" className="mt-2">Odwołaj wizytę</Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Jesteś pewien, że chcesz odwołać wizytę?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                          Tej akcji nie można cofnąć. <br/> 
+                          Możesz umówić nowy termin wizyty na ekranie głównym.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Anuluj</AlertDialogCancel>
+                        <AlertDialogAction>Odwołaj</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </AlertDescription>
+              </Alert>
+            )
+          })
+        :
+        visits && visits.length == 0 ? 
+          "Brak zaplanowanych wizyt"   :
+        <div>
+          <Alert>
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+          </Alert>
+        </div>
+      }
     </div>
-    </div>
+  </div>
   );
 }
