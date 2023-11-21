a8d61e1280bd1a9ca564362964c5bd1aac28d98c
Mateusz Zientek <mateusz-z1703@o2.pl>
2023-11-21 12:04:14 +0100

    fIx availableHours

--- backend/schemas/visit.js
+++ backend/schemas/visit.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const visitSchema = new mongoose.Schema({
-  day: { type: Date, required: true },
+  day: { type: String, required: true },
   startTime: { type: Number, required: true },
   duration: { type: Number, required: true },
   user_id: { type: String, required: true },
